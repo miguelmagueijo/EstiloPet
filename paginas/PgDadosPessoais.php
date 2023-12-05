0ccc546d11219d4f1ccbf5a28e242a18a77cf4a4
Miguel Magueijo <miguelmagueijo@hotmail.com>
2023-12-05 17:11:52 +0000

    Change edit user and personal data;
    Change editarDados.php, PgDadosPessoais.php, PgEditarUtilizador.php;

--- paginas/PgDadosPessoais.php
+++ paginas/PgDadosPessoais.php
@@ -1,86 +1,99 @@
-<!DOCTYPE html>
-<html lang="pt">
+<?php
+    include_once("auth.php");
+    redirectToIfNotLogged();
 
-<head>
-    <meta charset="UTF-8">
-    <title>Estilo Pet</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
-    <link rel="stylesheet" type="text/css" href="estilo.css" />
-</head>
+    /* @var $conn mysqli */
+    $stmt = $conn->prepare("SELECT * FROM user WHERE idUser = ?");
+    $stmt->bind_param("i", $_SESSION["userId"]);
 
-<body>
-    <?php
-    session_start();
-    $CURR_PAGE_NAME = "personal_data";
-
-    if (isset($_SESSION["utilizador"])) {
-
-        include('../basedados/basedados.h');
-
-        //variaveis de sessao
-        $idUser = $_SESSION["id"];
-
-        //vai buscar o id do utilizador
-        $sql = "SELECT * FROM user WHERE idUser = '" . $idUser . "'";
-
-        $res = mysqli_query($conn, $sql);
-
-        if (($row = mysqli_fetch_array($res)) != null) {
-            $nomeUser = $row["nomeUser"];
-            $morada = $row["morada"];
-            $email = $row["email"];
-            $telemovel = $row["telemovel"];
-
-
-            include_once("navbar.php");
-
-            echo "
-                <div id='container'>
-                    <div id='editar-dados-box'>
-                        <div id='login-cabecalho'>Editar Dados Pessoais</div>
-                        <form action='editarDados.php' method='POST'>
-                            <div class='input-div' id='input-form'>
-                                Nome de utilizador:
-                                <input type='text' name='username' value='" . $nomeUser . "'required/>
-                            </div>
-                            <div class='input-div' id='input-form'>
-                                Morada:
-                                <input type='text' name='morada' value='" . $morada . "'required/>
-                            </div>
-                            <div class='input-div' id='input-form'>
-                                E-mail:
-                                <input type='e-mail' name='email' value='" . $email . "'required/>
-                            </div>
-                            <div class='input-div' id='input-form'>
-                                Contacto:
-                                <input type='tel' name='telemovel' value='" . $telemovel . "'required/>
-                            </div>
-                            <div class='input-div' id='input-form'>
-                                Password:
-                                <input type='password' name='pass' />
-                            </div>
-                            <div class='input-div' id='input-form'>
-                                Confirmar Password:
-                                <input type='password' name='ConfPass' />
-                            </div>
-                            <div id='acoes'>
-                                <input type='hidden' name='idUser' value='" . $idUser . "'>
-                                <input type='submit' value='Guardar'>
-                            </div>
-                        </form>
-                    </div>
-                </div>";
-        } else {
-            echo "Algo correu mal...";
-            header("Refresh:1; url=PgUtilizador.php");
-        }
-    } else {
-        echo "Efetue login!";
-        header("Refresh:1; url=logout.php");
+    if (!$stmt->execute()) {
+        header("Refresh: 3; url=PgUtilizador.php");
+        die("<h1>Erro na consulta da base de dados...</h1>");
     }
 
-    include_once("footer.html");
-    ?>
-</body>
+    $res = $stmt->get_result();
 
+    if (!$res || $res->num_rows == 0) {
+        header("Refresh: 3; url=PgUtilizador.php");
+        die("<h2>Existiu um erro a encontrar os seus dados</h2>");
+    }
+
+    $userData = $res->fetch_assoc();
+
+    $CURR_PAGE_NAME = "personal_data";
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <title>Estilo Pet - Editar dados pessoais</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <link rel="stylesheet" type="text/css" href="estilo.css" />
+    </head>
+
+    <body>
+        <?php
+            include_once("navbar.php");
+
+            if (isset($_GET["bad_connection"])) {
+                echo "
+                        <div class='edit-content-warning error'>
+                            Não foi possível atualizar os dados
+                        </div>
+                    ";
+            } else if (isset($_GET["success"])) {
+                echo "
+                        <div class='edit-content-warning success'>
+                            Dados atualizados com sucesso
+                        </div>
+                    ";
+            } else if (isset($_GET["bad_connection_password"])) {
+                echo "
+                    <div class='edit-content-warning error'>
+                        As passwords não correspondem
+                    </div>
+                ";
+            } else if (count($_GET) >= 1) {
+                echo "
+                        <div class='edit-content-warning error'>
+                            Possui dados inválidos
+                        </div>
+                    ";
+            }
+        ?>
+        <div class="edit-content-container">
+            <h2>Editar Dados Pessoais</h2>
+            <form action="editarDados.php" method="POST">
+                <div class="input-box <?php echo isset($_GET['inv_nome']) ? 'invalid' : '' ?>">
+                    Nome de utilizador:
+                    <input type="text" name="username" value="<?php echo $userData['nomeUser'] ?>" required/>
+                </div>
+                <div class="input-box <?php echo isset($_GET['inv_morada']) ? 'invalid' : '' ?>">
+                    Morada:
+                    <input type="text" name="morada" value="<?php echo $userData['morada'] ?>" required/>
+                </div>
+                <div class="input-box <?php echo isset($_GET['inv_email']) ? 'invalid' : '' ?>">
+                    E-mail:
+                    <input type="email" name="email" value="<?php echo $userData['email'] ?>" required/>
+                </div>
+                <div class="input-box <?php echo isset($_GET['inv_telemovel']) ? 'invalid' : '' ?>">
+                    Contacto:
+                    <input type="tel" name="telemovel" value="<?php echo $userData['telemovel'] ?>" required/>
+                </div>
+                <div class="input-box <?php echo isset($_GET['inv_password']) ? 'invalid' : '' ?>">
+                    Password:
+                    <input type="password" name="pass" />
+                </div>
+                <div class="input-box <?php echo isset($_GET['inv_confirmacao_password']) ? 'invalid' : '' ?>">
+                    Confirmar Password:
+                    <input type="password" name="ConfPass" />
+                </div>
+                <button class="form-btn" type="submit">
+                    Guardar alterações
+                </button>
+            </form>
+        </div>
+    </body>
 </html>
--- paginas/editarDados.php
+++ paginas/editarDados.php
@@ -1,89 +1,88 @@
 <html>
-
-<head>
-    <meta charset="UTF-8">
-</head>
-
+    <head>
+        <meta charset="UTF-8">
+    </head>
 </html>
 <?php
-session_start();
+    include_once("auth.php");
+    redirectToIfNotLogged("logout.php");
 
-if (isset($_SESSION["utilizador"])) {
+    $userId = isset($_POST["idUser"]) ? $_POST["idUser"] : $_SESSION["userId"];
+    $editingOwnData = $userId == $_SESSION["userId"];
+    $invalidFields = array();
+    $redirectPage = $editingOwnData ? "PgDadosPessoais.php?" : "PgEditarUtilizador.php?idUser=$userId&";
 
-    include('../basedados/basedados.h');
-    include "tiposUtilizadores.php";
-
-    //variaveis de sessao
-    $idUser = $_SESSION["id"];
-    $tipoUtilizador = $_SESSION["tipo"];
-
-    //variaveis do formulario
-    $nomeUser = $_POST["username"];
-    $morada = $_POST["morada"];
-    $email = $_POST["email"];
-    $telemovel = $_POST["telemovel"];
-
-    $idCliente = $_POST["idUser"];
-
-    if ($idUser != $idCliente) {
-        if ($tipoUtilizador != ADMIN) {
-            echo "Ultilizador inválido";
-        } else if ($tipoUtilizador == ADMIN) {
-            $tipoUser = $_POST["tipoUtilizador"];
-
-            $update = "Update `user` 
-                        SET `nomeUser` = '" . $nomeUser . "', `morada` = '" . $morada . "', `email` = '" . $email . "', 
-                        `telemovel` = '" . $telemovel . "', `tipoUtilizador` = '" . $tipoUser . "'
-                        WHERE `idUser` = '" . $idCliente . "'";
-
-            $res = mysqli_query($conn, $update);
-
-            echo "Update com sucesso!";
-            header("Refresh:1; url=PgEditarUtilizador.php?idUser=$idCliente");
-        }
-    } else {
-        //restantes variaveis do formulario
-        $pass = $_POST["pass"];
-        $confPass = $_POST["ConfPass"];
-
-        if (!(empty($nomeUser) || empty($morada) || empty($email) || empty($telemovel))) {
-            if (!(empty($pass) && empty($confPass))) {
-                if ($pass != $confPass) {
-                    echo '<p>Passwords não correspondem!</p>';
-                    header("Refresh:1; url=PgDadosPessoais.php");
-                } else {
-                    $update = "Update `user` 
-                        SET `nomeUser` = '" . $nomeUser . "', `morada` = '" . $morada . "', `email` = '" . $email . "', `telemovel` = '" . $telemovel . "',
-                        `pass` =  '" . md5($pass) . "' 
-                        WHERE `idUser` = '" . $idUser . "'";
-
-                    $res = mysqli_query($conn, $update);
-
-                    echo "Update com sucesso!";
-                    header("Refresh:1; url=PgDadosPessoais.php");
-                }
-            } else {
-                $update = "Update `user` 
-                            SET `nomeUser` = '" . $nomeUser . "', `morada` = '" . $morada . "', `email` = '" . $email . "',
-                            `telemovel` = '" . $telemovel . "'
-                            WHERE `idUser` = '" . $idUser . "'";
-
-                $res = mysqli_query($conn, $update);
-
-                echo "Update com sucesso!";
-                header("Refresh:1; url=PgDadosPessoais.php");
-            }
-
-        } else {
-            echo "Os campos têm de estar todos preenchidos.";
-            header("Refresh:1; url=PgDadosPessoais.php");
-
-        }
-
+    if (!isset($_POST["username"]) || strlen($_POST["username"]) <= 2) {
+        $invalidFields[] = "inv_nome";
     }
 
-} else {
-    echo "Efetue login!";
-    header("Refresh:1; url=logout.php");
-}
+    if (!isset($_POST["morada"]) || strlen($_POST["morada"]) <= 2) {
+        $invalidFields[] = "inv_morada";
+    }
+
+    if (!isset($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        $invalidFields[] = "inv_email";
+    }
+
+    if (!isset($_POST["telemovel"]) || strlen($_POST["telemovel"]) < 9) {
+        $invalidFields[] = "inv_telemovel";
+    }
+
+    if (count($invalidFields) > 0) {
+        header("Location: $redirectPage".implode("&", $invalidFields));
+        die();
+    }
+
+    $username = $_POST["username"];
+    $address = $_POST["morada"];
+    $email = $_POST["email"];
+    $phoneNumber = $_POST["telemovel"];
+    $newUserType = $auth_userType;
+
+    /* @var $conn mysqli */
+    $stmt = null;
+    if (!$editingOwnData) {
+        redirectToIfNotAdmin("PgUtilizador.php");
+        $newUserType = isset($_POST["tipoUtilizador"]) ? $_POST["tipoUtilizador"] : $newUserType;
+        if ($newUserType != ADMIN && $newUserType != CLIENTE_POR_VALIDAR && $newUserType != CLIENTE && $newUserType != FUNC) {
+            header("Location: $redirectPage"."inv_tipoUtilizador");
+            die();
+        }
+    }
+
+    $stmt = $conn->prepare("UPDATE `user` SET `nomeUser` = ?, `morada` = ?, `email` = ?, `telemovel` = ?, `tipoUtilizador` = ? WHERE `idUser` = ?");
+    $stmt->bind_param("ssssii", $username, $address, $email, $phoneNumber, $newUserType, $userId);
+
+    if (!$stmt->execute()) {
+        header("Location: $redirectPage"."bad_connection");
+        die();
+    }
+
+    // Force same user check because admin cannot change user password in the website
+    if ($editingOwnData && isset($_POST["pass"]) && isset($_POST["ConfPass"]) && !empty($_POST["pass"]) && !empty($_POST["ConfPass"])) {
+        if (strlen($_POST["pass"]) <= 2) {
+            $invalidFields[] = "inv_password";
+        }
+
+        if (strlen($_POST["ConfPass"]) <= 2 || strcmp($_POST["pass"], $_POST["ConfPass"]) != 0) {
+            $invalidFields[] = "inv_confirmacao_password";
+        }
+
+        if (count($invalidFields) > 0) {
+            header("Location: $redirectPage".implode("&", $invalidFields));
+            die();
+        }
+
+        $pass = $_POST["pass"];
+
+        $stmt = $conn->prepare("UPDATE `user` SET `pass` = MD5(?) WHERE `idUser` = ?");
+        $stmt->bind_param("si", $pass, $userId);
+
+        if (!$stmt->execute()) {
+            header("Location: $redirectPage"."bad_connection_password");
+            die();
+        }
+    }
+
+    header("Location: $redirectPage"."success");
 ?>
